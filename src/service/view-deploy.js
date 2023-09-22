79b543c5d743eed903651cdc1d68eede9eb00e75
cleberson.chagas <clebrson.b.chagas@accenture.com>
2023-09-22 17:42:51 -0300

    clean code

--- src/service/view-deploy.js
+++ src/service/view-deploy.js
@@ -5,63 +5,38 @@ const { createComment } = require('./save-gitlab');
 
 
 function deployAndMonitor(params) {
-  try {
-    let org = params.org;
-    let id = params.jobid;
-    let message = params.isvalidate ? ' Validate ' : ' Deploy ';
+  let id = params.jobid;
+  let message = params.isvalidate ? ' Validate ' : ' Deploy ';
 
-      const conn = new jsforce.Connection();
+  const conn = new jsforce.Connection();
 
-      conn.initialize({
-        instanceUrl: params.instance,
-        accessToken: params.token
-      });
+  conn.initialize({
+    instanceUrl: params.instance,
+    accessToken: params.token
+  });
 
-      let callAgain= (error, result)=>{
-        console.log('result', result);
-          if(error){
-            console.error('O deploy falhou:');
-            console.error(JSON.stringify(error));
-            createComment(params.projectid, params.mrid, {msg:'O deploy falhou:', result: error});
+  let callAgain = (error, result) => {
+    if (error) {
+      console.error('O deploy falhou:');
+      console.error(JSON.stringify(error));
+      createComment(params.projectid, params.mrid, { msg: 'O deploy falhou:', result: error });
 
-          }
+    }
 
-          if(result && !result.done){
-            console.log('em andamento',params.instance, result.status, id);
+    if (result && !result.done) {
+      console.log('em andamento', params.instance, result.status, id);
 
-            conn.metadata.checkDeployStatus(id, true, callAgain);
-          }else if (result && result?.status === 'Succeeded') {
+      conn.metadata.checkDeployStatus(id, true, callAgain);
+    } else if (result && result?.status === 'Succeeded') {
 
-            createComment(params.projectid, params.mrid, `${message} concluído com sucesso!`);
-          }else if(result && result?.status === 'Failed'){
-            createComment(params.projectid, params.mrid, `O ${message} falhou: \n ${jsonArrayToMarkdownTable(result.details.componentFailures)}`);
-          }
-        }
-
-        conn.metadata.checkDeployStatus(id, true, callAgain);
-
-  //  });
-
-    //await conn.login();
-
-    // Define os detalhes do deploy
-    // const deployOptions = {
-    //   rollbackOnError: true,
-    //   checkOnly: false, // Defina como true se desejar testar o deploy sem efetivá-lo
-    //   testLevel: 'NoTestRun', // Altere de acordo com as suas necessidades de teste
-    // };
-
-    // Caminho do pacote que você deseja implantar (pode ser um arquivo ZIP ou diretório)
-    // const deployPath = '/caminho/do/seu/pacote';
-
-    // Inicia o deploy
-    // const deployResult = await conn.metadata.deploy(deployPath, deployOptions);
-
-    // Monitora o progresso do deploy
-  } catch (err) {
-    console.error('Erro durante o deploy:');
-    console.error(err);
+      createComment(params.projectid, params.mrid, `${message} concluído com sucesso!`);
+    } else if (result && result?.status === 'Failed') {
+      createComment(params.projectid, params.mrid, `O ${message} falhou: \n ${jsonArrayToMarkdownTable(result.details.componentFailures)}`);
+    }
   }
+
+  conn.metadata.checkDeployStatus(id, true, callAgain);
+
 }
 
 function jsonArrayToMarkdownTable(jsonArray) {
@@ -83,4 +58,4 @@ function jsonArrayToMarkdownTable(jsonArray) {
 }
 
 // Chamando a função para iniciar o deploy e monitorar
-module.exports = {deployAndMonitor};
+module.exports = { deployAndMonitor };
