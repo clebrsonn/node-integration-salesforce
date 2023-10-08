90d2cb7f9cb728d03d054261992c223ec88df78a
cleberson.chagas <cleberson.chagas@syngenta.com>
2023-10-08 11:34:53 -0300

    adjust in db location

--- src/db/db.js
+++ src/db/db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const database = new Sequelize({
     dialect: 'sqlite',
-    storage: 'src/db/t.db'
+    storage: './db/t.db'
 })
 
 module.exports = database;
