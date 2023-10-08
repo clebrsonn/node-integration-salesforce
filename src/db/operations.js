ae5353c7783cc0bd69f80d82cf019b6913d2871d
cleberson.chagas <cleberson.chagas@syngenta.com>
2023-10-08 12:18:02 -0300

    adjust in frontend

--- src/db/operations.js
+++ src/db/operations.js
@@ -1,5 +1,6 @@
 const Job = require('../models/Job')
 const socket = require("../service/create-socket");
+const {Op} = require("sequelize");
 
 function insert(params) {
     return Job.create(params);
@@ -9,7 +10,13 @@ function update(params, where) {
 
     Job.update(params,
         where).then( ()=> {
-            Job.findAll().then(result =>
+            Job.findAll({
+                where: {
+                    status:{
+                        [Op.notIn]: ["Success", "Error"]
+                    }
+                }
+            }).then(result =>
                 socket.getIO().emit('registry-update', result)
             );
 
