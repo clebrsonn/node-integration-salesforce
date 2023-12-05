d16eae8f38d77307744f5e011cbf07fa609ae62b
cleberson.chagas <clebrson.b.chagas@accenture.com>
2023-12-05 20:20:33 -0300

    added new comments in PR

--- src/service/view-deploy.js
+++ src/service/view-deploy.js
@@ -3,6 +3,9 @@ const { createComment } = require('./save-gitlab');
 const { notifyTeams } = require('./notify-teams');
 const jsonToMarkdown = require('json-to-markdown-table');
 const dbOperations = require('../db/operations');
+require('dotenv').config();
+
+
 
 function deployAndMonitor(params) {
     makeCall(params);
@@ -49,28 +52,70 @@ const callAgain= (params, result)=> {
   } if (result) {
     let action = result.checkOnly ? ' Validate ' : ' Deploy ';
 
-    let message = result?.status === 'Succeeded' ? `${action} concluído com sucesso!` :
-                  result?.status === 'Failed'    ? `O ${action} falhou: \n ${transform(result.details.componentFailures)}`: '';
+    let message = `${action} ${result?.status}! \n ${transform(result.details)}`
 
     // result.details.runTestResult.codeCoverage
     //
     // result.details.runTestResult.codeCoverageWarnings
 
     if(!params.commented){
-      if(result.success && result.details.runTestResult?.codeCoverageWarnings){
-        const covegare = result.details.runTestResult?.codeCoverageWarnings.filter((elem) =>
-        result.details.componentSuccesses?.find(el => elem.name === el.fullName) );
 
-        message += transform(covegare);
-
-      }
       createComment(params.jobId, params.projectId, params.mrId, message);
       notifyTeams();
     }
   }
 }
 function transform(jsonToTransform) {
+  if(!jsonToTransform){
+    return '';
+  }
+  let str='';
+  if(jsonToTransform.componentFailures){
+    str += '\n\r Component Failures \n\r';
+    str+= toMarkdown(jsonToTransform.componentFailures);
+  }
+  if(jsonToTransform.runTestResult.failures){
+    str += '\n\r Test Failures \n\r';
+    str+= toMarkdown(jsonToTransform.runTestResult.failures);
+
+  }
+  if(jsonToTransform.runTestResult.codeCoverageWarnings){
+    let coverage = jsonToTransform.runTestResult?.codeCoverageWarnings.filter((elem) =>
+    jsonToTransform.componentSuccesses?.find(el => elem.name === el.fullName) );
+    if(coverage){
+      str += '\n\r Coverage Test Class < 75% \n\r';
+
+      str += toMarkdown(coverage);
+    }
+
+  }
+  if(jsonToTransform.runTestResult.codeCoverage){
+    let coverage = jsonToTransform.runTestResult?.codeCoverage.filter((elem) =>
+    jsonToTransform.componentSuccesses?.find(el => elem.name === el.fullName) );
+    if(coverage){
+      let coverageNew=[];
+      for (let index = 0; index < coverage.length; index++) {
+        let element = coverage[index];
+        element['coveragePercent'] = ( 1- (parseInt(element.numLocationsNotCovered, 10) /parseInt(element.numLocations, 10)))* 100;
+        if(element['coveragePercent'] < process.env.COVERAGE){
+          coverageNew.push(element);
+        }
+      }
+      if(coverageNew.length>0){
+        str += '\n\r Coverage Test Class < 85% \n\r';
+
+        str += toMarkdown(coverageNew);
+      }
+
+    }
+
+  }
+  return str;
+}
+
+function toMarkdown(jsonToTransform){
   let columns;
+
   if(Array.isArray(jsonToTransform)){
     columns = Object.keys(jsonToTransform[0]);
   }else{
@@ -78,7 +123,7 @@ function transform(jsonToTransform) {
   }
 
 
-  return   jsonToMarkdown(jsonToTransform, columns);
+  return jsonToMarkdown(jsonToTransform, columns);
 
 }
 module.exports = { deployAndMonitor };
