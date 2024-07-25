e2770d4f700b008b6f1124b881c37b80955b19dc
Cleberson <clebrsonn@hotmail.com>
2024-07-25 15:51:50 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,8 +17,8 @@ import { AuthConfigModule } from './auth-config/auth-config.module';
     ConfigModule.forRoot({ isGlobal: true }),
     ScheduleModule.forRoot(),
     TypeOrmModule.forRoot({
-      //type: 'postgres',
-      type: 'sqlite',
+      type: 'postgres',
+      //type: 'sqlite',
       database: process.env.DB_ACCESS,
       // host: 'localhost',
       // port: 5432,
