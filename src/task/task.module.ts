872e1606e62edc7a92eba29cf43ffe5b6592f2fb
cleberson.chagas <cleberson.chagas@syngenta.com>
2025-02-14 18:48:54 -0300

    add HttpModule to task module imports

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -3,9 +3,10 @@ import { TasksService } from './task.service';
 import { JobsModule } from 'src/jobs/jobs.module';
 import { SfModule } from 'src/sf/sf.module';
 import { GitlabModule } from 'src/gitlab/gitlab.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [JobsModule, SfModule, GitlabModule],
+  imports: [JobsModule, SfModule, GitlabModule, HttpModule],
   providers: [TasksService],
   exports: [TasksService],
 })
